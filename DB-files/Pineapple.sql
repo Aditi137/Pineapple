f542f7f186f1b15bf48e17bf8dc8a2bb8ce5faf3
Filip Jäderberg <filipjad@kth.se>
2017-10-15 18:43:14 +0800

    Updated DB files

---
+++ DB-files/Pineapple.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Oct 15, 2017 at 12:42 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Pineapple`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Accounts`
+--
+
+CREATE TABLE `Accounts` (
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `id` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `account_type` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Accounts`
+--
+
+INSERT INTO `Accounts` (`name`, `email`, `id`, `password`, `account_type`) VALUES
+('Madeleine', 'mgartz@kth.se', '1234', 'ntu', '0'),
+('Filip', 'filipjad@kth.se', '1337', 'hejhej', '0'),
+('Helge Bengtsson', 'hej@123.se', 'HelgeZZ', 'komohjaelpmig', '1'),
+('made', 'mad@d.e', 'madekung', 'hejhej', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Relations`
+--
+
+CREATE TABLE `Relations` (
+  `s_id` varchar(20) NOT NULL,
+  `u_id` varchar(20) NOT NULL,
+  `confirmed` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Relations`
+--
+
+INSERT INTO `Relations` (`s_id`, `u_id`, `confirmed`) VALUES
+('1337', '1234', 'YES'),
+('madekung', '1337', 'no'),
+('HelgeZZ', '1337', 'NO'),
+('HelgeZZ', '1337', 'NO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Reminders`
+--
+
+CREATE TABLE `Reminders` (
+  `u_id` varchar(20) NOT NULL,
+  `s_id` varchar(20) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `details` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Accounts`
+--
+ALTER TABLE `Accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
